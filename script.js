dee9f27b8ccea46a75b3b61d262b751f77625e1f
adaryabegi <56917803+adaryabegi@users.noreply.github.com>
2020-03-24 00:28:59 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -537,7 +537,7 @@ var submit = document.getElementById('submitbtn').addEventListener('click', func
             
             }).then(function() {            
                 var email = {email:userEmail, warenkorb:dataAr, kontaktdaten:kontaktAr}
-                var json = JSON.stringify(email);
+                var json = JSON.stringify({data:email});
                 var xhr = new XMLHttpRequest();
             
                 xhr.open("POST", "https://www.bestellliste.com/functions/sendmail.php");
